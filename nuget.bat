4992b87c541efb8789129ef6913e91274a26f253
velio <velio.ivanov@gmail.com>
2015-04-25 15:56:27 +0300

    add sancastle help builder to nuget package fetching

--- nuget.bat
+++ nuget.bat
@@ -1,4 +1,5 @@
 @echo off
 cls
 ".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
+".nuget\NuGet.exe" "Install" "EWSoftware.SHFB" "-OutputDirectory" "packages" "-ExcludeVersion"
 pause
